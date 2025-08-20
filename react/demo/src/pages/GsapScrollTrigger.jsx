8cda6a311b2fce2defef197ac7193cf8c57d5bde
Bo01la <abdoehab30@gmail.com>
2025-08-21 02:00:59 +0300

    applied the (scrollTrigger and text) animation

--- react/demo/src/pages/GsapScrollTrigger.jsx
+++ react/demo/src/pages/GsapScrollTrigger.jsx
@@ -1,6 +1,44 @@
-const GsapScrollTrigger = () => {
-  // TODO: Implement the gsap scroll trigger
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+//ScrollTrigger is a plig-in. So, we import it from "gsap/all"
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
 
+// registering the plugin before the component:
+gsap.registerPlugin(ScrollTrigger);
+
+const GsapScrollTrigger = () => {
+  // we have a div wrapping the 2 elements we want to animate
+  // so we will use "useRef" to access elements inside the dive using
+  // gsap.utils.toArray()
+  // this methods guarantee that the elements inside the div will be sorted in an Array not a Node-List (this will help us to apply all Array method like.map or .forEach on the array)
+
+  const boxWrapperRef = useRef();
+  useGSAP(
+    () => {
+      // converted the elements inside the main wrapper to a clean array
+      const boxes = gsap.utils.toArray(boxWrapperRef.current.children);
+      boxes.forEach((box) => {
+        gsap.to(box, {
+          // x: 150,
+          x: 150 * (boxes.indexOf(box) + 2), // we can apply some math if we want
+          rotation: 360,
+          borderRadius: "100%",
+          scale: 1.5,
+          ease: "power1.inOut",
+          // activating scroll effect:
+          scrollTrigger: {
+            trigger: box, // Instead, this defines which element’s scroll position will control the animation.
+            // 👉 In this code, both happen to be the same element (box) — but they don’t have to be.
+            start: "bottom bottom", // start animation when the bottom of the box hits the bottom of screen
+            end: "top 20%", // end animation when the top of the box hits 20% of sceen
+            scrub: true, // makes animation smoother
+          },
+        });
+      });
+    },
+    { scope: boxWrapperRef } //Means: "Only watch elements inside this boxWrapperRef div, and clean them up when this component unmounts."
+  );
   return (
     <main>
       <h1>GsapScrollTrigger</h1>
@@ -50,8 +88,8 @@ const GsapScrollTrigger = () => {
           <path d="M5 12l7 7 7-7" />
         </svg>
       </div>
-
-      <div className="mt-20 w-full h-screen">
+        {/* adding our ref to the div wrapping the boxes we want to animate */}
+      <div ref={boxWrapperRef} className="mt-20 w-full h-screen"> 
         <div
           id="scroll-pink"
           className="scroll-box w-20 h-20 rounded-lg bg-pink-500"
